6dfab2ad0a3c0c83da34d8e204bbc461ba1c4789
이재용 <tomasjy27@gmail.com>
2023-09-20 13:07:19 +0900

    1-1 문자찾기

---
+++ src/main/java/algo/algorithm/Array/Finding_Characters.java
@@ -0,0 +1,34 @@
+package algo.algorithm.Array;
+
+import java.util.Scanner;
+
+public class Finding_Characters {
+
+    public int solution(String str, char t){
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+//        for(int i=0; i<str.length(); i++){
+//            if(str.charAt(i)==t){
+//                answer++;
+//            }
+//        }
+        for(char x : str.toCharArray()){
+            if(x == t){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Finding_Characters T = new Finding_Characters();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        char c = scanner.next().charAt(0);
+        System.out.println(T.solution(str, c));
+    }
+
+}
